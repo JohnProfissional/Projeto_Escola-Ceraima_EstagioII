37329e55a4958d45f4a1d203957c07af2b7c833e
JohnProfissional <johncba2015@hotmail.com>
2022-11-25 11:28:01 -0300

    Criando migration usuario

---
+++ database/migrations/2022_11_25_132030_create_usuario_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nomeusuario',100);
+            $table->string('cargo',100);
+            $table->string('email',100);
+            $table->string('senha',100);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
