7249090683bb8f699db09ca504ad1254ff5cfdc7
JohnProfissional <johncba2015@hotmail.com>
2022-11-25 22:38:49 -0300

    Criando migrations

---
+++ database/migrations/2022_11_25_173240_create_setores_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSetoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('setores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('descricaodasala',60);
+            $table->string('nomebem',60);
+            $table->string('numerodebem',);
+            $table->integer('quantidadedebem');
+            $table->unsignedBigInteger('saida_id');
+            $table->foreign('saida_id')->references('id')->on('saidas');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('setores');
+    }
+}
