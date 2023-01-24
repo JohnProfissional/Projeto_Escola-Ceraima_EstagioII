d51cd10347ffd36d15fbb2af5712af6a2a5910a2
JohnProfissional <johncba2015@hotmail.com>
2023-01-24 07:04:23 -0300

    index

---
+++ resources/views/emprestimos/index.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/styles.css'); }}">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=2.0">
+	<title>Document</title>
+</head>
+<body>
+@include('layouts.app')
+
+	<div id="header">
+			<h1>@yield('titulo')</h1>
+			<div id="nav">
+				<ul id="menu-h">
+                    <li><a href="{{route('home')}}">Home</a> </li>
+                    <li><a href="{{route('usuarios.index')}}">Usuarios</a> </li>
+                    <li><a href="{{route('setores.index')}}">Setores</a> </li>					
+					<li><a href="{{route('reservas.index')}}">Reservas</a> </li>					
+					<li><a href="{{route('patrimonios.index')}}">Patrimonios</a> </li>
+					<li><a href="{{route('patrimoniosinserviveis.index')}}">Patrimonios Inserviveis</a> </li>
+					<li><a href="{{route('manutencaos.index')}}">Manutenção</a> </li>
+                    <li><a href="{{route('desaparecidos.index')}}">Desaparecidos</a> </li>
+                    <li><a href="{{route('bensexcedentes.index')}}">Bens Excedentes</a> </li>
+					<li><a href="{{route('emprestimos.index')}}">Emprestimos </a> </li>
+				</ul>
+                
+               
+                
+                
+                
+                
+			</div>
+	</div>
+     <div class="card-body">
+                  @if($errors->any())
+                    <div class="alert alert-danger"> 
+                        <ul>
+                            @foreach($errors->all() as $error)
+                                <li>{{$error}}</li>
+                            @endforeach    
+                        </ul>   
+                    </div>        
+                @endif
+@section('titulo','Lista de Empréstimos')
+
+  <table class="table table-sm">
+ 
+   
+      @foreach ($Emprestimo as $emprestimos)
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th>nomedobememprestado</th><br>
+          <th>coddobem</th>
+          <th>nomedoprofissional</th>
+          <th>quantidadeemprestada</th>
+          <th>saida_id</th>
+                    
+        </tr>
+      </thead>
+       
+            <td scope="row">{{$emprestimos->id}}</td>
+
+          <td>{{$emprestimos->nome}}</td>
+          
+             <td>
+                   <form action="{{route('emprestimos.delete', ['id' => $emprestimos->id])}}" method="post">
+                    @csrf
+                    @method('DELETE')   
+                    <input type="submit" class="btn btn-primary" value="deletar">
+                    </form> 
+                </td>
+                <td>
+                   
+                </td>
+                <td>
+                    <form action="{{route('emprestimos.edit', ['id' => $emprestimos->id])}}" method="post">
+                        @csrf
+                        <input type="submit" class="btn btn-primary" name="formulario" value="alterar">
+                    </form>
+                </td>
+          
+          @endforeach
+     
+     
+    </table><br>
+
+
+     <form action="{{route('emprestimos.create')}}" method="post">
+                        @csrf
+                        <input type="submit" class="btn btn-primary" name="formulario" value="cadastrar novo empréstimo">
+                    </form>
+
+
+ </div>
+
+
+
+	 <h4>@yield('subtitulo')</h4>
+  <table class="col" id="row">
+
+  	 
+
+       <div class="row g-3">
+        <div class=col>
+
+        </div>
+
+       </div>
+
+
+  </table>
+<footer>
+			<br>
+			<br><br>
+            <div id="footer" align="center">
+			copyrigth @Sistema desenvolvido por Robério Fagundes dos Santos, John Junqueira
+            </div>
+</footer>
+</div>
+
+
+
+	</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+
+
+</body>
+</html>
