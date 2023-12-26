af4b3e6d79e43aac8b02a690170e90800eedd53d
JohnProfissional <johncba2015@hotmail.com>
2023-12-26 18:31:45 -0300

    Adição do botão SAIR no Menu em todas as telas das visualizações + renomeação do botão sair do cabeçalho para botão voltar + mudança de rotas desses botões + Ajustes

--- resources/views/emprestimos/edit.blade.php
+++ resources/views/emprestimos/edit.blade.php
@@ -25,8 +25,8 @@
 					<path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
 				</svg>
 			</a>
-			<a class="nav-item nav-link active align-itens-center m-2" href="{{ route('login')}}">
-				Sair
+			<a class="nav-item nav-link active align-itens-center m-2" href="{{ route('emprestimos.index')}}">
+				Voltar
 				<svg class="bi bi-box-arrow-right m-0" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
 					<path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
 					<path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
