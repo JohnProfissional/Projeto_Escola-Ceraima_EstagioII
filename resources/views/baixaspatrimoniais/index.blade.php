f2f255eb88c36d9739597877c49dec416caa0681
JohnProfissional <johncba2015@hotmail.com>
2023-12-26 16:49:49 -0300

    Adição do botão SAIR no Menu em todas as telas das views + Ajustes

--- resources/views/baixaspatrimoniais/index.blade.php
+++ resources/views/baixaspatrimoniais/index.blade.php
@@ -18,14 +18,14 @@
                     <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
                 </svg>
             </a>
-            <a class="nav-item nav-link align-itens-center m-2" href="{{ route('perfil') }}">
+            <a class="nav-item nav-link active align-itens-center m-2" href="{{ route('perfil') }}">
                 Perfil
                 <svg class="bi bi-person-circle m-0" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                 </svg>
             </a>
-            <a class="nav-item nav-link align-itens-center m-2" href="{{ route('login') }}">
+            <a class="nav-item nav-link active align-itens-center m-2" href="{{ route('login') }}">
                 Sair
                 <svg class="bi bi-box-arrow-right m-0" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
@@ -145,6 +145,11 @@
                         <i class="bi bi-diagram-2"></i>
                         Setores
                     </a>
+
+                    <a class="nav-link align-itens-left text-left mt-4 mb-4 ms-2 me-2 p-2 itens-menu-lateral" href="{{ route('logout')}}">
+                        <i class="bi bi-box-arrow-right"></i>
+                        Sair
+                    </a>
                 </div>
 
                 <div class="modal-footer">
@@ -173,7 +178,7 @@
         <div class="row">
             <div class="w-auto d-flex justify-content-center">
                 <!--CAMPO QUE DEVE SER MODIFICADO CONFORME INFORMAÇÕES DO BANCO-->
-                <h1>Baixas Patrimoniais</h1>
+                <!--<h1>Baixas Patrimoniais</h1>-->
             </div>
         </div>
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
                     <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
                 </svg>
             </a>
-            <a class="nav-item nav-link align-itens-center m-2" href="{{ route('perfil')}}">
+            <a class="nav-item nav-link active align-itens-center m-2" href="{{ route('perfil')}}">
                 Perfil
                 <svg class="bi bi-person-circle m-0" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
@@ -28,7 +28,7 @@
             <li class="nav-item">
                 <form action="{{ route('logout') }}" method="POST">
                     @csrf
-                    <a class="nav-item nav-link align-itens-center m-2" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
+                    <a class="nav-item nav-link active align-itens-center m-2" href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
                         Sair
                         <svg class="bi bi-box-arrow-right m-0" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
@@ -146,6 +146,11 @@
                     <a class="nav-link align-itens-left text-left mt-4 mb-4 ms-2 me-2 p-2 itens-menu-lateral" href="{{ route('setores.index') }}">
                         <i class="bi bi-diagram-2"></i>
                         Setores
+                    </a>
+
+                    <a class="nav-link align-itens-left text-left mt-4 mb-4 ms-2 me-2 p-2 itens-menu-lateral" href="{{ route('logout')}}">
+                        <i class="bi bi-box-arrow-right"></i>
+                        Sair
                     </a>
                 </div>
 
